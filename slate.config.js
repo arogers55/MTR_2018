73a641756366f3a313bf1287d824eb7b85e20796
Andrew Christensen <andreweskild@gmail.com>
2018-05-07 16:00:33 -0700

    Pushed version running new slate version 1.0

---
+++ slate.config.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-undef */
+
+const path = require('path');
+
+const alias = {
+  jquery: path.resolve('./node_modules/jquery'),
+  'lodash-es': path.resolve('./node_modules/lodash-es'),
+};
+
+module.exports = {
+  slateCssVarLoader: {
+    cssVarLoaderLiquidPath: ['src/snippets/css-variables.liquid'],
+  },
+  slateTools: {
+    extends: {
+      dev: {resolve: {alias}},
+      prod: {resolve: {alias}},
+    },
+  },
+  {
+    test: /\.(scss)$/,
+    use: [{
+      loader: 'style-loader', // inject CSS to page
+    }, {
+      loader: 'css-loader', // translates CSS into CommonJS modules
+    }, {
+      loader: 'postcss-loader', // Run post css actions
+      options: {
+        plugins: function () { // post css plugins, can be exported to postcss.config.js
+          return [
+            require('precss'),
+            require('autoprefixer')
+          ];
+        }
+      }
+    }, {
+      loader: 'sass-loader' // compiles Sass to CSS
+    }]
+  },
+};
